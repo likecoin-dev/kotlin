3ef1022a5ce327489f0ee98d3d26ccfe1a6729d7
Andrey Breslav <andrey.breslav@jetbrains.com>
2015-02-03 18:25:19 +0300

    `==` turned into a safe-call

--- core/builtins/native/kotlin/Library.kt
+++ core/builtins/native/kotlin/Library.kt
@@ -18,8 +18,6 @@ package kotlin
 
 public fun Any?.identityEquals(other: Any?): Boolean // = this === other
 
-public fun Any?.equals(other: Any?): Boolean
-
 // Returns "null" for null
 public fun Any?.toString(): String
 
